40f406387eea3ee5071ff2628ea0512973b52aea
cjeanm <jeanmichelcid@gmail.com>
2024-07-14 17:07:53 -0400

    gpush
    ansible setup
    
    ansible
    
    init.yml

---
+++ bin/.local/scripts/gpush.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gpush() {
+    git reset
+    git remote -v
+    git pull --rebase
+    git init
+    git add -A
+    git commit -m "-"
+    git branch -M main
+    git push origin main --force
+}
